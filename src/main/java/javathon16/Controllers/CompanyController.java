df2ebcd13d587d6a63da7cf85fbd63f5a9a55a75
Vasily <89313005745@mail.ru>
2019-01-27 00:15:01 +0300

    Update CompanyController.java

--- src/main/java/javathon16/Controllers/CompanyController.java
+++ src/main/java/javathon16/Controllers/CompanyController.java
@@ -2,22 +2,32 @@ package javathon16.Controllers;
 
 import javathon16.Models.Company;
 import javathon16.Repositories.CompanyRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CompanyController {
-
     CompanyRepository companyRepository;
 
     @Autowired
-    CompanyController(CompanyRepository companyRepository){
+    public CompanyController(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
-    }
-
+        companyRepository.save(new Company(1, "coffy", 5));}
     @GetMapping("/api/company/getbyid")
-    public Company getById(@RequestParam(value = "id") Long id){
+    public Company getCompanyById ( value = "id" int id){
         return companyRepository.findById(id).get();
     }
-
+    @PostMapping("/api/company/add")
+    public String addcompany(value = "name" String name,
+                             value = "id" int id,
+                             value ="maxBonus" int maxBonus) {
+        companyRepository.save(new Company(id, name, maxBonus));
+        System.out.println(companyRepository.findById(1));
+        return "qqq";
+    }
 }
