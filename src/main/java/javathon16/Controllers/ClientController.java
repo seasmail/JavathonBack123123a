d0bdf114241afad0522f105d50eb2fa8388b20c5
Semyon <voloshinsemyon@gmail.com>
2019-01-26 21:58:06 +0300

    123

--- src/main/java/javathon16/Controllers/ClientController.java
+++ src/main/java/javathon16/Controllers/ClientController.java
@@ -1,7 +1,11 @@
 package javathon16.Controllers;
 
+import javathon16.Models.Client;
 import javathon16.Repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +16,17 @@ public class ClientController {
     @Autowired
     public ClientController(ClientRepository clientRepository){
         this.clientRepository = clientRepository;
+    }
+
+    @GetMapping("/api/getClientById")
+    public Client getClientById(@RequestParam(value = "id") Long id){
+        return clientRepository.findById(id).get();
+    }
+
+    @GetMapping("/qqq")
+    public String qqq(){
+        clientRepository.save(new Client(999, "qwe"));
+        return "qqq";
     }
 
 
--- src/main/java/javathon16/Controllers/CompanyController.java
+++ src/main/java/javathon16/Controllers/CompanyController.java
@@ -5,9 +5,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CompanyController {
-
-    @RequestMapping("/q")
-    public String q(){
-        return "qqq";
-    }
 }
