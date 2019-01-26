78936f76c69bb74311acd3cf318f4bef0146b83f
Vasily <89313005745@mail.ru>
2019-01-26 23:48:31 +0300

    Update BonusController.java

--- src/main/java/javathon16/Controllers/BonusController.java
+++ src/main/java/javathon16/Controllers/BonusController.java
@@ -1,9 +1,45 @@
 package javathon16.Controllers;
 
+import javathon16.Models.Client;
 import javathon16.Repositories.BonusRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import javathon16.Repositories.ClientRepository;
+import javathon16.Models.Bonus;
+import javathon16.Repositories.CompanyRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
-public class BonusController{
+public class BonusController {
+    BonusRepository bonusRepository;
+
+    @Autowired
+    public BonusController(BonusRepository bonusRepository){
+        this.bonusRepository = bonusRepository;
+    }
+
+    @GetMapping("/api/client/getbyid")
+    public Bonus getBonusByClientIdAndCompanyName(@RequestParam(value = "id") Long clientId,
+                                                  @RequestParam(value = "id") Long companyId)
+    {
+        return bonusRepository.findById(clientId).get(); // TODO!!! Дописать запрос по двум параметрам
+    }
+
+    public String addBonus (@RequestParam( value = "id" ) int value){
+        bonusRepository.save(new Bonus(value));
+        return "Succsessfuly added new bonus with value " + value;
+    }
+    public String checkBonusAdge (@RequestParam(value = "id") Long clientId,
+                                  @RequestParam(value = "id") Long companyId)
+    {
+        if (clientRepository.findById(clientId).getBonus == companyRepository.findById(companyId).get().getMaxBonus)
+        {
+            clientRepository.findById(clientId).get().set(companyId);
+            return " You can get the prize!";
+        }
+        return "Not enoughth bonuses to get a prize!";
+    }
+
 }
