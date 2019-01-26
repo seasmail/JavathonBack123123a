07f76a161105510f75c88fe9fe23ac8e12cd12d6
Semyon <voloshinsemyon@gmail.com>
2019-01-26 21:43:06 +0300

    123

--- src/main/java/javathon16/Controllers/ClientController.java
+++ src/main/java/javathon16/Controllers/ClientController.java
@@ -1,4 +1,20 @@
 package javathon16.Controllers;
 
+import javathon16.Repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClientController {
+
+    ClientRepository clientRepository;
+
+    @Autowired
+    public ClientController(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+
+
+
+
 }
